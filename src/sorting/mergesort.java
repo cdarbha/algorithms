eada7ebe5d177ec4bdc98ceee096704017fc4e53
Chandrasekhar Darbha <chandrasekhar.darbha@gmail.com>
2018-05-29 08:59:53 +0530

    updated merge sort

--- src/sorting/mergesort.java
+++ src/sorting/mergesort.java
@@ -7,70 +7,57 @@ import utils.Utils;
 public class mergesort {
 
 	public static void main(String[] args) {
-		int[] arr = new int[] {9,5,-70,3,80,4,6,1,2, 100,50,75, 32, 45, 56, 12};
+		int[] arr = new int[] {9,5,-70,3,80,4,6,1,2, 100,50,75, 32, 45, 56};
 
-		int index1 = arr.length / 2;
-		int[] left = ArrayUtils.subarray(arr, 0, index1);
-		int[] right = ArrayUtils.subarray(arr, index1, arr.length);
-
-		int[] sorted = merge(left, right);
-
-		Utils.printArray(sorted);
+		Utils.printArray(sort(arr));
 	}
 
-	private static int[] merge(int[] arr1, int[] arr2) {
-
-		if(arr1.length == 1 && arr2.length == 1) {
-			if(arr1[0] < arr2[0])
-				return new int[] { arr1[0], arr2[0] };
-			else
-				return new int[] { arr2[0], arr1[0] };
-		}
-
-		int index1 = arr1.length / 2;
-		int[] left1 = ArrayUtils.subarray(arr1, 0, index1);
-		int[] right1 = ArrayUtils.subarray(arr1, index1, arr1.length);
-
-		int[] arr1Sorted = merge(left1, right1);
-
-		int index2 = arr2.length / 2;
-		int[] left2 = ArrayUtils.subarray(arr2, 0, index2);
-		int[] right2 = ArrayUtils.subarray(arr2, index2, arr2.length);
-
-		int[] arr2Sorted = merge(left2, right2);
-		int[] sorted = new int[arr1Sorted.length + arr2Sorted.length];
-
-		boolean notSorted = true;
-		int leftIndex = 0, rightIndex = 0;
-		int sortedIndex = 0;
+	private static int[] sort(int[] arr) {
+		if(arr.length == 1)
+			return arr;
 		
-		while(notSorted) {		
-			if(arr1Sorted[leftIndex] < arr2Sorted[rightIndex]) {
-				sorted[sortedIndex] = arr1Sorted[leftIndex];
+		int splitIndex = arr.length/2;
+		
+		int[] left = ArrayUtils.subarray(arr, 0, splitIndex);
+		int[] right = ArrayUtils.subarray(arr, splitIndex, arr.length);
+		
+		int[] sortedLeft = sort(left);
+		int[] sortedRight = sort(right);
+		
+		return merge(sortedLeft, sortedRight);		
+	}
+	
+	private static int[] merge(int[] left, int[] right) {
+		int[] merged = new int[left.length + right.length];
+		int leftIndex = 0, rightIndex = 0;
+		
+		for(int i = 0; i < merged.length; i++) {
+			if(leftIndex >= left.length && rightIndex >= right.length)
+				break;
+			else if(leftIndex >= left.length) {
+				merged[i] = right[rightIndex];
+				rightIndex++;
+				continue;
+			}
+			else if (rightIndex >= right.length) {
+				merged[i] = left[leftIndex];
+				leftIndex++;
+				continue;
+			}
+			
+			
+			if(left[leftIndex] <= right[rightIndex]) {
+				merged[i] = left[leftIndex];
 				leftIndex++;
 			}
 			else {
-				sorted[sortedIndex] = arr2Sorted[rightIndex];
+				merged[i] = right[rightIndex];	
 				rightIndex++;
 			}
 			
-			sortedIndex++;
 		}
 		
-		
-//		for(int i = 0, k = 0; i < sorted.length && k < arr2Sorted.length; i+=2,k++) {
-//			if(arr1Sorted[k] < arr2Sorted[k]) {
-//				sorted[i] = arr1Sorted[k];
-//				sorted[i+1] = arr2Sorted[k];
-//			}
-//			else {
-//				sorted[i] = arr2Sorted[k];
-//				sorted[i+1] = arr1Sorted[k];
-//			}
-//		}
-
-		Utils.printArray(sorted);
-		return sorted;
-
+		return merged;
 	}
+	
 }
